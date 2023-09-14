7238da99136e44a65939eb0b8031ce21df3ef97f
Natanet77 <natanet77@ukr.net>
2023-09-14 14:59:45 +0300

    end product

--- src/route/index.js
+++ src/route/index.js
@@ -27,25 +27,32 @@ class Product {
     this.#list.find((product) => product.id === id)
   }
 
-  static updateById = (id, data) => 
+  static updateById = (name, price, description) => {
+    const product = this.getById(id)
+    if (price) {
+      product.price = price
+    }
+    if (name) {
+      product.name = name
+    }
+    if (description) {
+      product.description = description
+    }
+  }
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
 }
-
-// router.get Створює нам один ентпоїнт
-
-// ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
-  })
-  // ↑↑ сюди вводимо JSON дані
-})
-
-// ================================================================
-
+//====================================================================
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
@@ -60,7 +67,7 @@ router.get('/product-create', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
-// ================================================================
+//=====================================================================
 
 // router.get Створює нам один ентпоїнт
 
@@ -85,20 +92,99 @@ router.post('/product-create', function (req, res) {
 
 // ================================================================
 
+//====================================================================
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/product-list', function (req, res) {
   // res.render генерує нам HTML сторінку
-
+  const list = Product.getList()
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('product-list', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'product-list',
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
+// ================================================================
+
+router.get('/product-edit', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const { id } = req.query
+
+  const product = Product.getById(Number(id))
+
+  // console.log(product)
+
+  if (product) {
+    // ↙️ cюди вводимо назву файлу з сontainer
+    return res.render('product-edit', {
+      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+      style: 'product-edit',
+
+      data: {
+        prodName: product.prodName,
+        price: product.price,
+        id: product.id,
+        description: product.description,
+      },
+    })
+  } else {
+    return res.render('alert', {
+      // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+      style: 'alert',
+      info: 'Товар з таким ID не знайдено',
+      link: '/product-edit  ',
+    })
+  }
+})
+// ↑↑ сюди вводимо JSON дані
+//==========================================================================
+router.post('/product-edit', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const { id, name, price, description } = req.body
+
+  Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+
+  console.log(Product.getList())
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+    info: 'Зберегти оновлення',
+  })
+
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+router.get('/product-delete', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const { id } = req.query
+
+  Product.deleteById(Number(id))
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+    info: 'Товар видалений',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
